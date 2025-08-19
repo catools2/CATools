67fde9956a20cdf5541b4ea9e9cdbef96b28aae3
akeshmiri <kimiak2000@gmail.com>
2025-08-18 19:22:08 -0500

    setEnableDownloads for drivers

---
+++ web.testng/src/test/java/org/catools/web/tests/CWebPageTest.java
@@ -0,0 +1,12 @@
+package org.catools.web.tests;
+
+import org.catools.web.drivers.CDriver;
+import org.testng.annotations.Test;
+
+public class CWebPageTest extends CWebTest<CDriver> {
+
+  @Test
+  public void buildPageWithValidField() {
+    getDriver().open("https://google.com");
+  }
+}
